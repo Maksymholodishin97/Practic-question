6c49989a50385d994ba4e8f649ba22b690c3bb32
Maksymholodishin97 <maksymholodishin97@gmail.com>
2026-04-01 22:47:59 +0300

    First commit

---
+++ src/question5/task1.java
@@ -0,0 +1,10 @@
+package question5;
+
+public class task1 {
+    int[] arr = new int[45];
+
+    for(int i = 0; i < arr.length; i++) {
+        arr[i] = i * 2 + 1;
+        System.out.println(arr[i]);
+    }
+}
